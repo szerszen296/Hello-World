e488894ee25d0b724ef3adf079eb362b43998189
Jakub Pentoś <jakub.pentos@tfbank.pl>
2025-08-04 08:33:07 +0200

    usniete pliki

--- helper.py
+++
@@ -1,2 +0,0 @@
-def test1(a, b):
-    return "test1 result"
--- main.py
+++
@@ -1,8 +0,0 @@
-from helper import test1
-
-def main():
-    result = test1(1, 2)
-    print(result)
-
-if __name__ == "__main__":
-    main()

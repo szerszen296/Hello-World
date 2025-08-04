66d4cdf88922aa1c775b897e54abc8c9eee58f15
Jakub Pentoś <jakub.pentos@tfbank.pl>
2025-08-04 09:06:20 +0200

    dodane funkcje obliczen

--- functions.py
+++ functions.py
@@ -1,2 +1,31 @@
-def test1(x, y):
-    return("test1")
+def sum(x, y):
+    if type(x) == str:
+        print("x should be a number") 
+    elif type(y) == str:
+        print("y should be a number")
+    else:
+        return(x + y)
+
+def sub(x, y):
+    if type(x) == str:
+        print("x should be a number") 
+    elif type(y) == str:
+        print("y should be a number")
+    else:
+        return(x - y)
+
+def multi(x, y):
+    if type(x) == str:
+        print("x should be a number") 
+    elif type(y) == str:
+        print("y should be a number")
+    else:
+        return(x * y)
+
+def div(x, y):
+    if type(x) == str:
+        print("x should be a number") 
+    elif type(y) == str:
+        print("y should be a number")
+    else:
+        return(x / y)
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from functions import *
+
+x = 2
+y = 2
+
 print("Hello World")
 
 def main():
-    return("main")
+    print(sum(x, y))
+    print(sub(x, y))
+    print(multi(x, y))
+    print(div(x, y))
 
 if __name__ == '__main__':
     main()
